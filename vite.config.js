ebe1044dc052070b5efaf45a3327c4c3f8494484
wangdinghui <153421240@qq.com>
2026-07-09 09:42:16 +0800

    微信访问通过

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import uni from "@dcloudio/vite-plugin-uni";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
+  publicDir: "public",
   plugins: [uni()],
   resolve: {
     alias: {
